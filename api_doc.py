6647eb3aeaa80d2b5df319daba802a4fdba64e35
KaijieJiang <njukjjiang@gmail.com>
2018-10-30 17:59:05 -0400

    update the api doc

--- api_doc.py
+++ api_doc.py
@@ -1,22 +1,14 @@
-def create_database():
-    """
-    call this function to create database, if the database already exists, it will do nothing
-    :return: no return
-    """
-
-def create_table():
-    """
-    create two tables inventory and transaction
-    inventory(product_type, remains)
-    transaction(transaction_id, product_type, number)
-    if the table already exists, it will drop the table and create a new one
-    :return: no return
-    """
-
 def getmaxid():
     """
     get the largest transaction_id from transaction table, first time call it will return 1000
     :return: the largest transaction_id
+    """
+
+def getremains(product_type):
+    """
+    get remain number of a certain product
+    :param product_type: the type you want to query
+    :return: the number of this product
     """
 
 def insert_transaction(dic):
@@ -24,34 +16,6 @@ def insert_transaction(dic):
     insert a transaction record to the transaction table
     will ignore duplicated transaction id and catch exception
     :param dic: a dictionary with fields (transaction_id, product_type, number)
-    :return: no return
+    :return: a direction which shows the remain number of the product you insert
+             (product_type, number). number = -1 if the insert operation is wrong
     """
-
-def insert_inventory():
-    """
-    insert inventory information(shoes with 100 remains & pants with 100 remains),
-    should be called only once at the creation of the table
-    :return:
-    """
-
-example:
-
-
-	create_database()
-    create_table()
-    insert_inventory()
-    dic = {
-        'transaction_id': 0,
-        'product_type': 'shoes',
-        'number': 7
-    }
-    dic['transaction_id'] = getmaxid() + 1
-    insert_transaction(dic)
-    dic = {
-        'transaction_id': 0,
-        'product_type': 'pants',
-        'number': 8
-    }
-    dic['transaction_id'] = getmaxid() + 1
-    insert_transaction(dic)
-    insert_transaction(dic)
