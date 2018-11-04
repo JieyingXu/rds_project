94512b53b17566998884d0218c8c28e25e732d2e
JieyingXu <jieyingx@andrew.cmu.edu>
2018-11-04 17:01:21 -0500

    achieved proxy forward and fetch make_transaction request and reponse to and from server

--- proxy/proxy_server/views.py
+++ proxy/proxy_server/views.py
@@ -1,21 +1,59 @@
 from django.shortcuts import render
 from proxy_server.forms import *
+from django.http import HttpResponse
 import requests
 
 # Create your views here.
+SERVER_IP_LIST = ["http://127.0.0.1:8000"]
 
 def home(request):
-    response = requests.get("http://127.0.0.1:8000/home")
-    json_res = response.json()
-    context = {}
-    context["form"] = TransactionForm()
-    context["shoes_number"] = json_res["shoes_number"]
-    context["pants_number"] = json_res["pants_number"]
-    return render(request, 'proxy_server/index.html', context)
+    try:
+        response = requests.get(SERVER_IP[0]+"/home")
+        if response.status_code == requests.codes.ok:
+            json_res = response.json()
+            context = {}
+            context["form"] = TransactionForm()
+            context["shoes_number"] = json_res["shoes_number"]
+            context["pants_number"] = json_res["pants_number"]
+            return render(request, 'proxy_server/index.html', context)
+        else:
+            return HttpResponse("Server error", content_type="text/plain")
+    except ValueError:
+        return HttpResponse("Json Decode Failed", content_type="text/plain")
+    except:
+        return HttpResponse("Server Connection Failed", content_type="text/plain")
+
 
 def make_transaction(request):
-    return render(request, 'proxy_server/index.html', {})
+    if request.method == 'POST':
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            product_type, number = form.cleaned_data['product_type'], form.cleaned_data['product_number']
+            try:
+                payload = {'product_type':product_type, 'product_number':number}
+                response = requests.post(SERVER_IP[0]+'/make_transaction', data=payload)
+                json_res = response.json()
+                context = {}
+                context["form"] = TransactionForm()
+                context["shoes_number"] = json_res["shoes_number"]
+                context["pants_number"] = json_res["pants_number"]
+                if json_res['code'] == -1:
+                    context["message"] = 'Database Error: Insertion Failed!'
+                else:
+                    context["message"] = 'Transaction: %s of %s Succeeded!' % \
+                    (number, product_type)
+                return render(request, 'proxy_server/index.html', context)
+            except ValueError:
+                return HttpResponse("Json Decode Failed", content_type="text/plain")
+            except:
+                return HttpResponse("Server Connection Failed", content_type="text/plain")
+    else:
+        return HttpResponse("Make Transaction Cannot Accept GET Request", content_type="text/plain")
 
 def detect(request):
-    return render(request, 'proxy_server/index.html', {})
+    try:
+        response = requests.get(SERVER_IP[0]+'/detect')
+
+    except:
+
 
--- server/online_shop/views.py
+++ server/online_shop/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from online_shop.rds_mysql_apis import *
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def home(request):
@@ -16,34 +18,30 @@ def home(request):
     print("From server:", context)
     return JsonResponse(context)
 
+@csrf_exempt
 def make_transaction(request):
-    # if request.method == 'POST':
-    #     form = TransactionForm(request.POST)
-    #     if form.is_valid():
-    #         transaction_row = {}
-    #         transaction_row['transaction_id'] = get_maxid() + 1
-    #
-    #         product_type, number = form.cleaned_data['product_type'], form.cleaned_data['product_number']
-    #         transaction_row['product_type'] = product_type
-    #         transaction_row['number'] = int(number)
-    #         response = insert_transaction(transaction_row)
-    #
-    #         remain_shoes_number = get_remains("shoes")
-    #         remain_pants_number = get_remains("pants")
-    #         context = HttpResponse()
-    #         context["form"] = TransactionForm()
-    #         context["shoes_number"] = remain_shoes_number
-    #         context["pants_number"] = remain_pants_number
-    #
-    #         if response['number'] == -1:
-    #             context["message"] = 'Database Error: Insertion Failed!'
-    #         else:
-    #             context["message"] = 'Transaction: %s of %s Succeeded!' % \
-    #             (number, product_type)
-    #         return context
-    return HttpResponse("Alive", content_type="text/plain")
+    if request.method == 'POST':
+        product_type = request.POST.get('product_type')
+        product_number = request.POST.get('product_number')
+        print("From Server: product type->%s, product number->%s" % (product_type, product_number))
+        transaction_row = {}
+        transaction_row['transaction_id'] = get_maxid() + 1
+        transaction_row['product_type'] = product_type
+        transaction_row['number'] = int(product_number)
+
+        response = insert_transaction(transaction_row)
+        remain_shoes_number = get_remains("shoes")
+        remain_pants_number = get_remains("pants")
+
+        context = {}
+        context["code"] = response['number']
+        context["shoes_number"] = remain_shoes_number
+        context["pants_number"] = remain_pants_number
+        print("From server:", context)
+        return JsonResponse(context)
 
 def detect(request):
     # print("I am alive!")
+    context = {'code': '1'}
     return HttpResponse("Alive", content_type="text/plain")
 
