3439cbbaba1f45a7a24b4efecdf66fbd96c5d0f9
JieyingXu <jieyingx@andrew.cmu.edu>
2018-11-04 15:59:24 -0500

    achieved proxy forward home request to server

--- proxy/proxy_server/views.py
+++ proxy/proxy_server/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from proxy_server.forms import *
+import requests
 
 # Create your views here.
+
+def home(request):
+    response = requests.get("http://127.0.0.1:8000/home")
+    json_res = response.json()
+    context = {}
+    context["form"] = TransactionForm()
+    context["shoes_number"] = json_res["shoes_number"]
+    context["pants_number"] = json_res["pants_number"]
+    return render(request, 'proxy_server/index.html', context)
+
+def make_transaction(request):
+    return render(request, 'proxy_server/index.html', {})
+
+def detect(request):
+    return render(request, 'proxy_server/index.html', {})
+
